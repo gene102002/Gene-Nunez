f7c4b1e90d44f6d69cd7737d4497f5de1c233117
unknown <genesins013@gmail.com>
2026-02-27 04:16:31 +0800

    Created Services section and project section finish

--- script/script.js
+++ script/script.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
+    $('#next_Project').click(function () {
+        $('#Jolliekod').hide(1000);
+        $('#Jolliekod_Description').hide(1000);
+        $('#Gene_Description').fadeIn(1000);
+        $('#Gene').fadeIn(1000);
+        
+    });
+    $('#previous_Project').click(function () {
+        $('#Jolliekod').fadeIn(1000);
+        $('#Jolliekod_Description').fadeIn(1000);
+        $('#Gene_Description').hide(1000);
+        $('#Gene').hide(1000);
+       
+    });
 
     
+
 
 });
