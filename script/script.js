f5609f2d2c4073a885c020bcfdbbcb16582a4290
unknown <genesins013@gmail.com>
2026-02-27 13:41:59 +0800

    added feedback method

--- script/script.js
+++ script/script.js
@@ -1,4 +1,13 @@
+// Email configuration
+const SERVICE_ID = "service_icquqog";
+const TEMPLATE_ID = "template_lit7jfj";
+const PUBLIC_KEY = "BJnKvuMe_on36HQxQ";
+
+// Initialize EmailJS
+emailjs.init(PUBLIC_KEY);
+
 $(document).ready(function () {
+    
     $('#next_Project').click(function () {
         $('#Jolliekod').hide(1000);
         $('#Jolliekod_Description').hide(1000);
@@ -14,7 +23,25 @@ $(document).ready(function () {
        
     });
 
-    
-
-
 });
+
+// Global function for HTML onclick
+function sendFeedback() {
+    const feedback = $('#feedback').val();
+    
+    if (!feedback.trim()) {
+        alert('Please enter feedback before submitting.');
+        return;
+    }
+
+    emailjs.send(SERVICE_ID, TEMPLATE_ID, {
+        message: feedback
+    }).then(function(response) {
+        console.log('Email sent successfully!', response);
+        alert('Thank you! Your feedback has been sent.');
+        $('#feedback').val('');
+    }, function(error) {
+        console.error('Failed to send email:', error);
+        alert('Sorry, there was an error sending your feedback. Please try again.');
+    });
+}
